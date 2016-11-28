8f6ed44cf244d1898461719eb4b9964c9886b6b9
cam330 <wilcoxcam@gmail.com>
2016-11-28 14:24:25 -0500

    Initial Commit

---
+++ myGuideUserStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myGuideUserStore
+//
+//  Created by Cameron Wilcox on 11/28/16.
+//  Copyright © 2016 Cameron Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
