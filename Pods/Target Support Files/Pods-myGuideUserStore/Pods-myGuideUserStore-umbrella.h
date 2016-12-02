e1b87478198dadd98b8a3a692b17d7af2fed19a9
cam330 <wilcoxcam@gmail.com>
2016-12-02 08:25:12 -0800

    getting data from firebase

---
+++ Pods/Target Support Files/Pods-myGuideUserStore/Pods-myGuideUserStore-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_myGuideUserStoreVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_myGuideUserStoreVersionString[];
+
--- myGuideUserStore/ViewController.swift
+++ myGuideUserStore/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+//    let storage = FIRStorage.storage()
+    let ref = FIRDatabase.database().reference()
+    var tourArray: NSMutableArray = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func getData(_ sender: Any) {
+        
+        let conditionRef = ref.child("tours").child("france")
+        
+        conditionRef.observe(FIRDataEventType.value, with: {(snapshot) in
+//            let postDict = snapshot.value as? [String: AnyObject] ?? [:]
+            var tempItems = [NSDictionary]()
+//            print("TEST1",tempItems)
+            for item in snapshot.children {
+                let child = item as! FIRDataSnapshot
+//                print("TEST2",child)
 
+            }
+            
+            print("COMPLETE")
+        })
+        
+        
+        
+        conditionRef.observe(.value, with: { snapshot in
+//            print(snapshot.value ?? String())
+            
+            self.tourArray.add(snapshot.value ?? String())
+            
+//            let passingArray = (((self.tourArray.value(forKey: "array")) as AnyObject).value(forKey: "audio"))
+            
+            let passingArray = ((((snapshot.value as AnyObject).value(forKey: "array")) as AnyObject).value(forKey: "audio"))
+            
+            let partitionArray = (passingArray as! NSArray?)![1]
+            
+            var base64String : String!
+            
+            print(partitionArray)
+            
+//            print("TEST3",passingArray ?? String())
+        })
+    }
+    
+    @IBAction func showData(_ sender: Any) {
+        print(self.tourArray)
+    }
 
 }
 
